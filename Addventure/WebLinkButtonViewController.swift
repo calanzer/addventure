6fe08ae73fa3bb2213a992f3e1889b2a65776753
clanzer <clanzer@luc.edu>
2017-04-26 13:34:31 -0500

    Final updates

---
+++ Addventure/WebLinkButtonViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WebLinkButtonViewController.swift
+//  Addventure
+//
+//  Created by Christian  on 4/25/17.
+//  Copyright © 2017 Chrstian Lanzer. All rights reserved.
+//
+
+import UIKit
+
+class WebLinkButtonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
